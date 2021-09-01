54a1de1fa38ae14ace75a858095b2ab8096bed28
João Bruno Mélo de Alencar <joaobrunoalencar@gmail.com>
2021-08-31 22:05:44 -0300

    add dockerfile and config of redis service

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('how are you doing?');
+  res.send('Hi there');
 });
 
 app.listen(3000, () => {
